9f17851d5d47c5678effd89b675a4643e27e07bc
Raphael-Cruz <55248086+Raphael-Cruz@users.noreply.github.com>
2025-08-31 22:46:47 -0300

    started some routes, installed express dependecy

---
+++ routes/shopRouter.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const shopRouter = express.Router();
+shopRouter
+  .route("/")
+  .all((req, res, next) => {
+    res.statusCode = 200;
+    res.setHeader("Content-Type", "text/plain");
+    next();
+  })
+  .get((req, res) => {
+    res.end("Will generate the shop to you");
+  })
+  .post((req, res) => {
+    res.end(
+      `Will add the shop: ${req.body.name} with description: ${req.body.description}`
+    );
+  })
+  .put((req, res) => {
+    res.statusCode = 403;
+    res.end("PUT operation not supported on /shop");
+  })
+  .delete((req, res) => {
+    res.end("Deleting all shop");
+  });
+
+
+
+module.exports = shopRouter;
